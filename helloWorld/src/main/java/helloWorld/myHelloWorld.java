6186b98c292701bf29296d4d52b6f5f9e59edaa1
Mollie Bowers <mbowers@progrexion.com>
2019-03-11 16:00:58 -0600

    add bugs

--- helloWorld/src/main/java/helloWorld/myHelloWorld.java
+++ helloWorld/src/main/java/helloWorld/myHelloWorld.java
@@ -11,8 +11,15 @@ public class myHelloWorld {
         frame.setSize(200, 200);
         frame.setTitle("Hello world");
         frame.setVisible(true);
-
+        
         return 0;
+	}
+	
+	private boolean method1() {
+		
+        String blockCount = String.valueOf((int)Math.ceil(237 + 1)/10) ;
+
+		return true;
 	}
 
 }
